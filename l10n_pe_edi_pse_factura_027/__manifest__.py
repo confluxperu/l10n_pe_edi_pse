c5480d36250f2b070417730d10b42c7e6bc0aa9d
Edwar Ruben <edwardmf94@gmail.com>
2026-07-14 18:28:57 -0500

    [FIX] l10n_pe_edi_pse_factura_027: Correccion en codigo de versionado

--- l10n_pe_edi_pse_factura_027/__manifest__.py
+++ l10n_pe_edi_pse_factura_027/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'EDI for Peru with PSE - Detracciones de transporte 027',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'summary': 'Electronic Invoicing for Peru using direct connection with PSE - Detracciones de transporte 027',
     'category': 'Accounting/Localizations/EDI',
     'author': 'Obox',
