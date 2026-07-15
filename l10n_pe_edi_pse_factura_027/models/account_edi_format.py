1952503050cc1767e27e6dc65caed9bd8883f16a
Edwar Ruben <edwardmf94@gmail.com>
2026-07-14 19:40:01 -0500

    [FIX] l10n_pe_edi_pse_factura_027: Ajuste de etiquetas de version 15->19, campos de sunat actualizados

--- l10n_pe_edi_pse_factura_027/models/account_edi_format.py
+++ l10n_pe_edi_pse_factura_027/models/account_edi_format.py
@@ -10,26 +10,26 @@ class AccountEdiFormat(models.Model):
     def _l10n_pe_edi_get_edi_values_conflux(self, invoice):
         res = super(AccountEdiFormat, self)._l10n_pe_edi_get_edi_values_conflux(invoice)
         if invoice.l10n_pe_dte_dettran_origin_address_id:
-            res["detraction_origin_address_street"]=(invoice.l10n_pe_dte_dettran_origin_address_id.street or '') \
+            res["direccion_origen"]=(invoice.l10n_pe_dte_dettran_origin_address_id.street or '') \
                     + (invoice.l10n_pe_dte_dettran_origin_address_id.district_id and ', ' + invoice.l10n_pe_dte_dettran_origin_address_id.district_id.name or '') \
                     + (invoice.l10n_pe_dte_dettran_origin_address_id.province_id and ', ' + invoice.l10n_pe_dte_dettran_origin_address_id.province_id.name or '') \
                     + (invoice.l10n_pe_dte_dettran_origin_address_id.state_id and ', ' + invoice.l10n_pe_dte_dettran_origin_address_id.state_id.name or '') \
                     + (invoice.l10n_pe_dte_dettran_origin_address_id.country_id and ', ' + invoice.l10n_pe_dte_dettran_origin_address_id.country_id.name or '')
-            res["detraction_origin_address_zip"]=invoice.l10n_pe_dte_dettran_origin_address_id.district_id.code
+            res["ubigeo_origen"]=invoice.l10n_pe_dte_dettran_origin_address_id.district_id.code
         if invoice.l10n_pe_dte_dettran_delivery_address_id:
-            res["detraction_delivery_address_street"]=(invoice.l10n_pe_dte_dettran_delivery_address_id.street or '') \
+            res["direccion_destino"]=(invoice.l10n_pe_dte_dettran_delivery_address_id.street or '') \
                     + (invoice.l10n_pe_dte_dettran_delivery_address_id.district_id and ', ' + invoice.l10n_pe_dte_dettran_delivery_address_id.district_id.name or '') \
                     + (invoice.l10n_pe_dte_dettran_delivery_address_id.province_id and ', ' + invoice.l10n_pe_dte_dettran_delivery_address_id.province_id.name or '') \
                     + (invoice.l10n_pe_dte_dettran_delivery_address_id.state_id and ', ' + invoice.l10n_pe_dte_dettran_delivery_address_id.state_id.name or '') \
                     + (invoice.l10n_pe_dte_dettran_delivery_address_id.country_id and ', ' + invoice.l10n_pe_dte_dettran_delivery_address_id.country_id.name or '')
-            res["detraction_delivery_address_zip"]=invoice.l10n_pe_dte_dettran_delivery_address_id.district_id.code
+            res["ubigeo_destino"]=invoice.l10n_pe_dte_dettran_delivery_address_id.district_id.code
 
         if invoice.l10n_pe_dte_dettran_val_ref_serv_trans:
-            res["detraction_val_ref_serv_trans"]=invoice.l10n_pe_dte_dettran_val_ref_serv_trans
+            res["val_ref_serv_trans"]=invoice.l10n_pe_dte_dettran_val_ref_serv_trans
         if invoice.l10n_pe_dte_dettran_val_ref_carga_efec:
-            res["detraction_val_ref_carga_efec"]=invoice.l10n_pe_dte_dettran_val_ref_carga_efec
+            res["val_ref_carga_efec"]=invoice.l10n_pe_dte_dettran_val_ref_carga_efec
         if invoice.l10n_pe_dte_dettran_val_ref_carga_util:
-            res["detraction_val_ref_carga_util"]=invoice.l10n_pe_dte_dettran_val_ref_carga_util
+            res["val_ref_carga_util"]=invoice.l10n_pe_dte_dettran_val_ref_carga_util
         if invoice.l10n_pe_dte_dettran_detalle_viaje:
-            res["detraction_detalle_viaje"]=invoice.l10n_pe_dte_dettran_detalle_viaje
+            res["detalle_viaje"]=invoice.l10n_pe_dte_dettran_detalle_viaje
         return res
