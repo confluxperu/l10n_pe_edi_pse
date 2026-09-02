dddda7fb704e7370a72fdd31ea23d7b0012fd1e4
Edwar Ruben <edwardmf94@gmail.com>
2026-09-02 18:24:20 -0500

    [FIX] l10n_pe_Edi_pse_itgrupo: Compatibilizacion de campo de provincia a backoffice de partner itgrupo

--- l10n_pe_edi_pse_itgrupo/models/logistic_despatch.py
+++ l10n_pe_edi_pse_itgrupo/models/logistic_despatch.py
@@ -16,7 +16,7 @@ class LogisticDespatch(models.Model):
                                 + (self.origin_address_id.street_number2 and (' ' + self.origin_address_id.street_number2) or '') \
                                 + (self.origin_address_id.street2 and (' ' + self.origin_address_id.street2) or '') \
                                 + (self.origin_address_id.district_id and ', ' + self.origin_address_id.district_id.name or '') \
-                                + (self.origin_address_id.city_id and ', ' + self.origin_address_id.city_id.name or '') \
+                                + (self.origin_address_id.province_id and ', ' + self.origin_address_id.province_id.name or '') \
                                 + (self.origin_address_id.state_id and ', ' + self.origin_address_id.state_id.name or '') \
                                 + (self.origin_address_id.country_id and ', ' + self.origin_address_id.country_id.name or ''),
             'destino_ubigeo': self.delivery_address_id.district_id.code if self.delivery_address_id.district_id else False,
@@ -25,7 +25,7 @@ class LogisticDespatch(models.Model):
                                 + (self.delivery_address_id.street_number2 and (' ' + self.delivery_address_id.street_number2) or '') \
                                 + (self.delivery_address_id.street2 and (' ' + self.delivery_address_id.street2) or '') \
                                 + (self.delivery_address_id.district_id and ', ' + self.delivery_address_id.district_id.name or '') \
-                                + (self.delivery_address_id.city_id and ', ' + self.delivery_address_id.city_id.name or '') \
+                                + (self.delivery_address_id.province_id and ', ' + self.delivery_address_id.province_id.name or '') \
                                 + (self.delivery_address_id.state_id and ', ' + self.delivery_address_id.state_id.name or '') \
                                 + (self.delivery_address_id.country_id and ', ' + self.delivery_address_id.country_id.name or ''),
         })
